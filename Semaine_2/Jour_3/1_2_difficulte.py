8bb84cd3732c240f7b0a3c986e12153480f77bfa
Alain-75 <alain75@alain75.com>
2019-01-23 14:09:02 +0100

    Better 1.2

--- Semaine_2/Jour_3/1_2_difficulte.py
+++ Semaine_2/Jour_3/1_2_difficulte.py
@@ -13,13 +13,18 @@ MAX_TARGET = (2**16 -1) * 2**208
 # number takes up, and the next one to three bytes give the most significant digits of the
 # number. If the 2nd byte has a value greater than 127 then the number is interpreted as being
 # negative.
+
+# Bits = 0xWWXXYYZZ
+# WW => nombre d'octet dans la valeur.
+# XXYYZZ => 3 octets les plus significatifs de la valeur.
+# Par exemple, si WW = 0x05 le nombre cible est sur 5 octets, et égal à 0x XX YY ZZ 00 00
 def convert(hex_string):
 	if "0x" == hex_string[0:2]:
 		hex_string = hex_string[2:]
-	number_of_bytes = int(hex_string[0:2], 16) - 3
-	decoded = bytes.fromhex(hex_string[2:])
-	value = int.from_bytes(decoded, byteorder='big', signed=False)
-	return value * 256**number_of_bytes
+
+	number_of_bytes = int(hex_string[0:2], 16)
+	decoded = bytes.fromhex(hex_string[2:] + ("00" * (number_of_bytes-3))) # -3 car XX YY ZZ sont déjà compté
+	return int.from_bytes(decoded, byteorder='big', signed=False)
 
 
 def compute_difficulty(hex_target):

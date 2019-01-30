5d8dcf665e56042e588c00967481999e1010afca
Alain-75 <alain75@alain75.com>
2019-01-30 11:59:40 +0100

    Deuxième version scène libre

--- Semaine_3/Jour_3/scene_libre.sol
+++ Semaine_3/Jour_3/scene_libre.sol
@@ -1,16 +1,17 @@
-pragma solidity ^0.4.25
+pragma solidity ^0.4.25;
 
 contract SceneLibre
 {
 	uint constant MAX_CRENEAUX = 12;
 	string[MAX_CRENEAUX] _passage_artistes;
 	uint _nb_creneaux_libres = MAX_CRENEAUX;
+	uint _artiste_courant = 0;
 
 	function est_inscrit(string nom_artiste) public view returns(bool)
 	{
 		for (uint i = 0; i < MAX_CRENEAUX; ++i)
 		{
-			if (_passage_artistes[i] == nom_artiste)
+			if (keccak256(bytes(_passage_artistes[i])) == keccak256(bytes(nom_artiste)))
 			{
 				return true;
 			}
@@ -19,13 +20,23 @@ contract SceneLibre
 		return false;
 	}
 
-	function inscrire(string nom_artiste)
+	function inscrire(string nom_artiste) public
 	{
-		require(nom_artiste != "", "Nom vide non valide.")
+		require(bytes(nom_artiste).length > 0, "Nom vide non valide.");
 		require(false == est_inscrit(nom_artiste), "Un artiste est deja inscrit sous ce nom.");
 		require(_nb_creneaux_libres > 0, "Tous les creneaux sont pris.");
 
 		_passage_artistes[MAX_CRENEAUX - _nb_creneaux_libres] = nom_artiste;
 		_nb_creneaux_libres -= 1;
+	}
+
+	function artiste_courant() public view returns(string nom_artiste)
+	{
+		return _passage_artistes[_artiste_courant];
+	}
+
+	function passer_prochain_artiste() public
+	{
+		_artiste_courant += 1;
 	}
 }

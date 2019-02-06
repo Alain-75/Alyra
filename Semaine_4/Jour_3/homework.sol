ed6afca52873bff1cb5acfbde7f9eef2fd669b14
Alain-75 <alain75@alain75.com>
2019-02-06 15:39:59 +0100

    Exercice 4.1.1

---
+++ Semaine_4/Jour_3/homework.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.5.3;
+
+import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+contract Credibility
+{
+	using SafeMath for uint256;
+
+	mapping (address => uint256) public _cred;
+
+	bytes32[] private _homeworks;
+
+	function produce_hash(string memory homework) public pure returns(bytes32 homework_hash)
+	{
+		return keccak256(homework);
+	}
+
+	function transfer(address recipient, uint256 credibility) public
+	{
+		// entails that _cred[msg.sender] > 0
+		require(_cred[msg.sender] > credibility, "Sender is not credible enough.");
+		require(_cred[recipient] > 0, "Recipient is not credible.");
+
+		_cred[msg.sender] -= credibility; // underflow not possible
+		_cred[recipient] = _cred[recipient].add(credibility);
+	}
+
+	function turn_in(bytes32 homework_hash) public returns(uint8 position)
+	{
+		require(_cred[msg.sender] == 0, "Already turned in your homework.");
+		uint nb_already_turned_in = _homeworks.length;
+
+		for (uint i = 0; i < nb_already_turned_in; i++)
+		{
+			require(homework_hash != _homeworks[i], "Cheater!");
+		}
+
+		_homeworks.push(homework_hash);
+
+		if (nb_already_turned_in > 1)
+		{
+			_cred[msg.sender] = 10;
+		}
+		else if (nb_already_turned_in == 1)
+		{
+			_cred[msg.sender] = 20;
+		}
+		else if (nb_already_turned_in == 0)
+		{
+			_cred[msg.sender] = 30;
+		}
+
+		return nb_already_turned_in + 1;
+	}
+}
